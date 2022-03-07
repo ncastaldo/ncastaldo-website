65ed82ecae48654e1d197da5b40d0ef8c2fdbe16
Nicola Castaldo <nicolacastaldo123@gmail.com>
2022-03-07 20:28:45 +0100

    about shorter

--- src/components/about/AboutDescription.vue
+++ src/components/about/AboutDescription.vue
@@ -7,8 +7,8 @@ export default {};
 <template>
   <div class="about-description">
     <p>
-      Hi, welcome to my personal website! My name is Nicola Castaldo and I come
-      from Udine, Italy.
+      Hi, welcome to my personal website! My name is
+      <b>Nicola Castaldo</b> and I come from <b>Udine, Italy</b>.
       <!--BaseTooltip>
         <template #source>Udine, Italy</template>
         <template #target
@@ -17,16 +17,13 @@ export default {};
       </BaseTooltip-->
     </p>
     <p>
-      I am a curious person and I always try to put passion in what I do. As a
-      child, I used to play a lot with the LEGO pecies to create everyday a new
-      building. I chose to study and work in the Computer Science field because
-      for me it's just like playing LEGO: with my hands I can make awesome
-      things everyday.
+      I am a <b>curious</b> person and I always try to put <b>passion</b> in
+      what I do.
     </p>
     <p>
       If you scroll down a bit, you can find some information about my
-      <b>Skills</b>, my professional and academic <b>Journey</b>, as well as the
-      details of some <b>Projects</b> that I worked on.
+      <b>Skills</b>, my professional and academic <b>Journey</b> and the details
+      of some <b>Projects</b> that I worked on.
     </p>
   </div>
 </template>
