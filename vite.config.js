8a03d264c87b2fe41c2a660862d2048d75a756ed
Nicola Castaldo <nicolacastaldo123@gmail.com>
2022-05-13 16:18:05 +0200

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/ncastaldo-website/" : "",
+  base: mode === "production" ? "/ncastaldo-website/" : "/",
   plugins: [vue()],
 }));
